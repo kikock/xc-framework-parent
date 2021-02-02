b069a2507378361b27a1017a954abc3055438fe4
kikock <kikock@qq.com>
2021-02-02 16:15:31 +0800

    增加课程管理模块

---
+++ xc-service-manage-course/src/main/java/com/xuecheng/manage_course/ManageCourseApplication.java
@@ -0,0 +1,25 @@
+package com.xuecheng.manage_course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+
+/**
+ *
+ * @project_name: xc-service-manage-course
+ * @description:  课程管理模块
+ * @create_name: kikock
+ * @create_date: 2021/2/2 16:05
+ *
+ **/
+@SpringBootApplication
+@EntityScan("com.xuecheng.framework.domain.course")//扫描实体类
+@ComponentScan(basePackages={"com.xuecheng.api"})//扫描接口
+@ComponentScan(basePackages={"com.xuecheng.manage_course"})
+@ComponentScan(basePackages={"com.xuecheng.framework"})//扫描common下的所有类
+public class ManageCourseApplication {
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(ManageCourseApplication.class, args);
+    }
+}
