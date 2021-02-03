a8b7b197ad976e6436785a0419d8f85ee14ff3e9
kikock <kikock@qq.com>
2021-02-03 10:59:26 +0800

    更新依赖

--- xc-service-manage-course/src/main/java/com/xuecheng/manage_course/dao/CourseBaseRepository.java
+++ xc-service-manage-course/src/main/java/com/xuecheng/manage_course/dao/CourseBaseRepository.java
@@ -5,12 +5,10 @@ import com.xuecheng.framework.domain.course.CourseBase;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 /**
- *
  * @project_name: xc-service-manage-course
- * @description: 课程管理基础dao  jpa处理数据库
+ * @description: 课程管理dao  jpa处理数据库
  * @create_name: kikock
  * @create_date: 2021/2/2 16:08
- *
  **/
 public interface CourseBaseRepository extends JpaRepository<CourseBase,String> {
 }
